e271d06cbcb2f2d1bc8512e9e6a013cbdefb182c
tone <anthonyschwartzcode@gmail.com>
2015-10-19 10:48:56 -0700

    Add Resources Method

--- pin_clone/app/controllers/pins_controller.rb
+++ pin_clone/app/controllers/pins_controller.rb
@@ -1,34 +1,43 @@
 class PinsController < ApplicationController
   def index
-    @pins = Pin.all
+    @pins = Pin.all.order(:id)
     render :index
   end
+
   def new
     @pin = Pin.new
   end
+
   def create
     @pin = Pin.create pin_params
     redirect_to "/pins"
   end
+
   def show
     @pin = Pin.find(params[:id])
   end
+
   def edit
     @pin = Pin.find(params[:id])
   end
+
   def update
     @pin = Pin.find(params[:id])
 
     @pin.update(pin_params)
     redirect_to '/pins'
   end
+
   def destroy
     @pin = Pin.find(params[:id])
     @pin.destroy
     redirect_to '/'
   end
-  private 
+
+  private # insures that you can't modify the form maliciously 
+
   def pin_params
     params.require(:pin).permit(:url, :name, :image)
   end
+
 end
